a65188c21595a8cf514b5bfee7a0f3bfea4770e6
Yaroslav Klimenko <klimyarik13@gmail.com>
2025-01-27 22:47:59 +0200

    Update googleOAuth2.js

--- src/utils/googleOAuth2.js
+++ src/utils/googleOAuth2.js
@@ -1,12 +1,12 @@
 import { OAuth2Client } from 'google-auth-library';
 import { env } from './env.js';
-import { P } from 'pino';
+
 import createHttpError from 'http-errors';
 
 const oauthConfig = {
   web: {
     client_id: env('GOOGLE_AUTH_CLIENT_ID'),
-    project_id: env('financemanager-449113'),
+    project_id: 'financemanager-449113',
     auth_uri: 'https://accounts.google.com/o/oauth2/auth',
     token_uri: 'https://oauth2.googleapis.com/token',
     auth_provider_x509_cert_url: 'https://www.googleapis.com/oauth2/v1/certs',
